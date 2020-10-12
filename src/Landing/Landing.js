5291dadfc1eb1ffdd3e016bb4072de95d44cee5d
rachel-tilley <racheletilley@outlook.com>
2020-10-12 17:27:28 -0400

    Initial file setup

---
+++ src/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Landing.css';
+
+function Landing() {
+  return (
+    <div className="Landing">
+      <header className="Landing-header">
+        <p>Welcome to our project</p>
+      </header>
+    </div>
+  );
+}
+
+export default Landing;
