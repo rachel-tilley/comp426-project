3536d3884eb08915a19f37d76e3a96e8e8b2ec22
rachel-tilley <racheletilley@outlook.com>
2020-10-12 18:56:48 -0400

    Set up bulma and began framing out landing

--- src/Frontend/Landing/Landing.js
+++ src/Frontend/Landing/Landing.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import './Landing.css';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Section, Container, Hero, Heading, Navbar, Button } from 'react-bulma-components';
 
 function Landing() {
   return (
     <div className="Landing">
-      <header className="Landing-header">
-        <p>Welcome to our project</p>
-      </header>
+      <Navbar>
+        <Navbar.Container>
+        <Navbar.Item href="#">
+            Postings
+          </Navbar.Item>
+          <Navbar.Item href="#">
+            Calendar
+          </Navbar.Item>
+          <Navbar.Item href="#">
+            Location Recommendations
+          </Navbar.Item>
+        </Navbar.Container>
+        <Navbar.Container position="end">
+          <Navbar.Item>
+            <Button.Group>
+              <Button color="is-primary">
+                Sign up
+              </Button>
+              <Button color="is-primary">
+                Log in
+              </Button>
+
+            </Button.Group>
+          </Navbar.Item>
+        </Navbar.Container>
+      </Navbar>
+
+      <Section>
+        <Hero color="info" >
+          <Hero.Body>
+            <Container>
+              <Heading>
+              Find a Study Buddy!
+              </Heading>
+              <Heading subtitle size={3}>
+              The #1 place for UNC students to connect
+              </Heading>
+            </Container>
+          </Hero.Body>
+        </Hero>
+      </Section>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Landing/Landing';
+import App from './Frontend/Landing/Landing';
 
 ReactDOM.render(
   <React.StrictMode>
